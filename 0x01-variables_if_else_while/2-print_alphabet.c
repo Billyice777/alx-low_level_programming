2d132eeaebba7c699816268c24944486252b9cc7
Billy777 <uwemedimoekpewohp@gmail.com.com>
2022-12-09 18:38:05 -0800

    az

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		c++;
+	}
+	putchar("\n");
+	return (0)
+}
