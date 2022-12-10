997f8b44d177de5fac8e675f2f173ddd36e5223f
Billy777 <uwemedimoekpewohp@gmail.com.com>
2022-12-09 20:17:40 -0800

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,9 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'e' && letter != 'q')
-			putchar(letter);
-	}
+		putchar(letter);
+	for (letter != 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 	putchar('\n');
 
 	return (0);
