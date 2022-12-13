58ef936c3ef7c328703fd2e1b4049a34894798af
Billy777 <uwemedimoekpewohp@gmail.com.com>
2022-12-13 13:29:43 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char la = 'a';
+
+	while (la <= 'z')
+	{
+		_putcahr(la);
+		la++
+	}
+	_putchar('\n');
+}
