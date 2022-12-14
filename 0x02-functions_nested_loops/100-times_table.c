bc028ff315ad7702e1c64d5e2940c2376321de0e
Billy777 <uwemedimoekpewohp@gmail.com.com>
2022-12-14 12:56:53 -0800

    life tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,7 +26,8 @@ void print_table(int n)
 				if (prod <= 9)
 					_putchar(' ');
 				if (prod >= 100)
-					_putchar((prod / 100) = '0');
+				{
+					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 			}
 			else if (prod <= 99 && prod >= 10)
